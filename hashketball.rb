a22517bab05d50dc97e041bdb4df08c7e8295ad1
Yahya  Gok <ygok17@hotmail.com>
2020-02-14 17:28:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,7 +200,7 @@ def big_shoe_rebounds
   rebounds = 0 
   game_hash.each do |place,team|
     team[:players].each do |attribute, data|
-      bingding.pry
+      binding.pry
         data[:players].each do |player|
         
           if player[:shoe] > shoe
